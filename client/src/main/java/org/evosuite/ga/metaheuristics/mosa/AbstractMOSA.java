500e02c7621050297ca73d803a18111ddafb9310
Mitchell Olsthoorn <mitchell.olsthoorn@outlook.com>
2020-05-12 22:50:30 +0200

    Add grammer-based fuzzing to EvoSuite
    By adding a grammer-based fuzzer to EvoSuite, more meaningfull tests can
    be produced by introducing more context specific seeds. This commit adds
    a fuzzer for JSON. It uses EvoSuite to generate the test setup and
    replaces some of the primites of EvoSuite by input from the fuzzer.

--- client/src/main/java/org/evosuite/ga/metaheuristics/mosa/AbstractMOSA.java
+++ client/src/main/java/org/evosuite/ga/metaheuristics/mosa/AbstractMOSA.java
@@ -44,6 +44,8 @@ import org.evosuite.utils.Randomness;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.natpryce.snodge.JsonMutator;
+
 import java.util.*;
 
 /**
@@ -162,6 +164,11 @@ public abstract class AbstractMOSA extends GeneticAlgorithm<TestChromosome> {
 			this.removeUnusedVariables(offspring1);
 			this.removeUnusedVariables(offspring2);
 
+			if (Properties.FUZZER) {
+				evolveInput(offspring1);
+				evolveInput(offspring2);
+			}
+
 			// apply mutation on offspring1
 			this.mutate(offspring1, parent1);
 			if (offspring1.isChanged()) {
@@ -199,6 +206,51 @@ public abstract class AbstractMOSA extends GeneticAlgorithm<TestChromosome> {
 		}
 		logger.info("Number of offsprings = {}", offspringPopulation.size());
 		return offspringPopulation;
+	}
+
+	/**
+	 * This method evolves the String primitives in an offspring with a grammar-based fuzzer.
+	 * @param offspring
+	 */
+	private void evolveInput(TestChromosome offspring) {
+		TestCase testCase = offspring.getTestCase();
+
+		// For each test case in the test suite mutate the strings
+		for (Statement statement: testCase) {
+			//logger.error("{}", statement.toString());
+
+			// Only apply the mutation on the String primitives
+			if (statement instanceof StringPrimitiveStatement) {
+				StringPrimitiveStatement stringStatement = (StringPrimitiveStatement) statement;
+
+				// Only apply the mutation with a probability of FUZZER_PROBABILITY
+				Random r = new Random();
+				if (r.nextDouble() <= Properties.FUZZER_PROBABILITY) {
+
+					// Fuzzer seed
+					String mutant = Properties.FUZZER_SEED;
+
+					// Apply JSON mutator
+					JsonMutator mutator = new JsonMutator();
+
+					int rounds = r.nextInt(Properties.FUZZER_MAX_MUTATION_ROUNDS);
+					for (int i = 0; i <= rounds; i++) {
+						Optional<String> strings = mutator.forStrings().mutate(mutant, 1).findFirst();
+						if (strings.isPresent()) {
+							mutant = strings.get();
+						}
+					}
+
+					// Replace input with mutated input
+					stringStatement.setValue(mutant);
+
+					// Manually tell the fitness function that the testcase has been changed. So, new a fitness value has to be computed
+					offspring.setChanged(true);
+				}
+				//logger.error("{}", stringStatement.getValue());
+			}
+		}
+		//logger.error("{}", testCase.toCode());
 	}
 
 	/**
@@ -350,6 +402,13 @@ public abstract class AbstractMOSA extends GeneticAlgorithm<TestChromosome> {
 
 		// Create a random parent population P0
 		this.generateInitialPopulation(Properties.POPULATION);
+
+		// Mutate input
+		if (Properties.FUZZER) {
+			for (TestChromosome t : this.population) {
+				this.evolveInput(t);
+			}
+		}
 
 		// Determine fitness
 		this.calculateFitness();
